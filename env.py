a5238365d528781d0413dea98212447af66ef7b5
kim young kwon <yatta.kim@gmail.com>
2019-07-26 02:13:06 +0900

    modify env

--- env.py
+++ env.py
@@ -7,6 +7,8 @@ class Env():
     def __init__(self):
         self.env_data = pd.read_csv("KOSPI_F_30_1.csv")
 
-
+    def getOnes(self):
+        pass
+        
 if __name__ == '__main__':
-    main()
+    Env()
