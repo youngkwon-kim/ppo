d55a05feb68f82480a57ed37cc64e5677b267d26
youngkwonkim <yatata.kim@gmail.com>
2019-07-27 15:34:43 +0900

    m2 env.py

--- env.py
+++ env.py
@@ -5,8 +5,22 @@ class Env():
     def __init__(self):
         self.env_data = pd.read_csv("KOSPI_F_30_1.csv")
 
+    def reset(self):
+        pass
+
     def getOnes(self):
         pass
         
+    def step(self, a):
+        if(a == 0):
+            r = 0
+        elif(a == 1):
+            r = -1
+        elif(a == 2):
+            r = -1
+
 if __name__ == '__main__':
-    Env()
+    env = Env()
+    env.reset()
+    a = 1
+    st = env.step(a)
