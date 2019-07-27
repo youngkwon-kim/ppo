66922d85dfbb949a5176060a7b947d273f4a6b49
kim young kwon <yatta.kim@gmail.com>
2019-07-28 06:55:27 +0900

    align ppo.py

--- env.py
+++ env.py
@@ -7,11 +7,15 @@ class Env():
         self.cnt = 0
         self.pre_a = 0
         self.done = False
-        self.env_data = pd.read_csv("KOSPI_F_30_1.csv")
+        self.env_data = pd.read_csv("KOSPI_F_30_1.csv").dropna()
+        self.env_data = self.env_data.sort_index(ascending=False)
 
     def reset(self):
         self.cnt = 0
         self.pre_a = 0
+        self.done = False
+        self.cnt = self.cnt + 1
+        return self.env_data.iloc[self.cnt].values
 
     def getOnes(self):
         self.cnt = self.cnt + 1
@@ -32,7 +36,7 @@ class Env():
 
 if __name__ == '__main__':
     env = Env()
-    env.reset()
+    s = env.reset()
     for a in [1,0,2,2,0,0,0,0,0,1,2,2,0,1,1,1,2]:
         st = env.step(a)
         print(st)
