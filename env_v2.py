e332efc0525192070ac082b356818d06a5e0284d
kim young kwon <yatta.kim@gmail.com>
2019-08-19 08:52:47 +0900

    env v2

--- env_v2.py
+++ env_v2.py
@@ -24,7 +24,7 @@ class Env():
         self.done = False
         info, s_prime = self.getOnes()
         if(info):
-            sp = [s_prime[0],s_prime[1],s_prime[2],s_prime[3],(s_prime[4] - s_prime[5]), 0]
+            sp = [0,0,0,0,0,0]
         else:
             sp = [0,0,0,0,0,0]
         sp3 = np.array(sp)
@@ -39,8 +39,16 @@ class Env():
             re = [True, [sp[0],sp[1],sp[2],sp[3],(sp[4] - sp[5])]]
         except:
             re = [False,[0,0,0,0,0]]
+        print(self.cnt, sp[0])
         return re
      
+    def setPosition(self,position,value):
+        self.position = position
+        self.position_value = value
+
+    def getPosition(self):
+        return self.position, self.position_value
+
     def step(self, a):
         info, s_prime = self.getOnes()
 
@@ -49,22 +57,23 @@ class Env():
         else:
             pass
 
+        reword = 0
 
-
-        sp.append(self.position)
-        sp2 = np.array(sp)
+        s_prime.append(self.position)
+        sp2 = np.array(s_prime)
         #print(torch.from_numpy(sp2).float())
         if(a != 0):
             self.pre_a = a
-        self.total = self.total + reword
-        return sp2, r, self.done, info
+        #self.total = self.total + reword
+        return sp2, reword, self.done, info
 
 if __name__ == '__main__':
     env = Env()
     s = env.reset()
     print(s)
-    for a in [1,2,1,2,1,2,2,2,1,2,0,0,1,2,1,2,2,2,0,0,1,1,1,2]:
-        sp,r,done,posiotion = env.step(a)
-        print(sp,r,done,posiotion)
-        if(done == True):
-            env.reset()
+    for i in range(30):
+        for a in [1,2,1,2,1,2,2,2,1,2,0,0,1,2,1,2,2,2,0,0,1,1,1,2]:
+            sp,r,done,info = env.step(a)
+            print(sp,r,done,info)
+            if(done == True):
+                env.reset()
